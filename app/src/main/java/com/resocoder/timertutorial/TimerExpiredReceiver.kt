91e05512320d6715d110e97ae1314a93d8829a02
mikeumus <mikeumus@gmail.com>
2018-09-24 20:35:00 -0400

    Even more things broken now.

--- app/src/main/java/com/resocoder/timertutorial/TimerExpiredReceiver.kt
+++ app/src/main/java/com/resocoder/timertutorial/TimerExpiredReceiver.kt
@@ -5,13 +5,22 @@ import android.content.Context
 import android.content.Intent
 import com.resocoder.timertutorial.util.NotificationUtil
 import com.resocoder.timertutorial.util.PrefUtil
+//import android.support.v4.content.ContextCompat.startActivity
+import android.text.format.Time
+
 
 class TimerExpiredReceiver : BroadcastReceiver() {
+    val WAKE: String = "Wake up" // https://discuss.kotlinlang.org/t/unresolved-reference-another-kotlin-class/3115/2?u=mikeumus
 
     override fun onReceive(context: Context, intent: Intent) {
         NotificationUtil.showTimerExpired(context)
 
         PrefUtil.setTimerState(TimerActivity.TimerState.Stopped, context)
         PrefUtil.setAlarmSetTime(0, context)
+
+        val myAct = Intent(context, TimerActivity::class.java)
+        myAct.putExtra(WAKE, true)
+        myAct.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        context.startActivity(myAct)
     }
 }
