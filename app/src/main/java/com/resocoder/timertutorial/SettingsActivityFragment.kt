d9b5537541f06bcb671321f6004ca6dd9e085d3c
mikeumus <mikeumus@gmail.com>
2018-09-25 00:42:12 -0400

    Using all androidx layouts now works!

--- app/src/main/java/com/resocoder/timertutorial/SettingsActivityFragment.kt
+++ app/src/main/java/com/resocoder/timertutorial/SettingsActivityFragment.kt
@@ -1,10 +1,11 @@
 package com.resocoder.timertutorial
 
 import android.os.Bundle
-import android.support.v7.preference.PreferenceFragmentCompat
+//import android.support.v7.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceFragment
 
 
-class SettingsActivityFragment : PreferenceFragmentCompat() {
+class SettingsActivityFragment : PreferenceFragment() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.preferences)
     }
